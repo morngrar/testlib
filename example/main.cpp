39bdc573ee6ac7ee9759e0ea4016b0f7d197ede8
Svein-Kåre Bjørnsen <sveinkare@gmail.com>
2018-05-27 22:23:04 +0200

    Made example file current

--- example/main.cpp
+++ example/main.cpp
@@ -1,27 +1,55 @@
 #include <iostream>
+#include <string>
 #include "testing.h"
 
+
+// Function to be  tested
 int add(int a, int b)
 {
     return a + b;
 }
 
-bool test_add()
+
+// Testing function -- must return true on pass, and false on fail.
+bool testing_add()
 {
     int ret  = add(2, 5);
     if (ret != 7) {
-        std::cerr << "add() --> 2 + 5 should be 7. Was " << ret << std::endl;
+        tst::failed_report( // Prettifies output. See header file.
+            "add()",
+            "2 + 5 should be 7.",
+            std::to_string(ret)
+        );
         return false;
     } else {
         return true;
     }
 }
 
+bool another_test()
+{
+    int ret = add(4, 4);
+    if (ret != 8) {
+        tst::failed_report(
+            "add()",
+            "4 + 4 should be 8.",
+            std::to_string(ret)  // this requires C++11 compilation (or later)
+        );
+        return false;
+    } else {
+        return true;
+    }
+}
+
+
 int main()
 {
     tst::TestStack stack;
-    stack.push(test_add);
-    stack.run_tests();
+
+    stack.push(testing_add);
+    stack.push(another_test);
+
+    stack.run_tests();  // Runs tests and outputs result.
 
     return 0;
 }
